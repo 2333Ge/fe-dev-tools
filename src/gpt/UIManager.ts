7c8054d1dc22ef409553d7f6abc4304c4636abfe
2333Ge <ych.class@qq.com>
2023-06-08 21:05:36 +0800

    fix:本地资源问题

--- src/gpt/UIManager.ts
+++ src/gpt/UIManager.ts
@@ -1,5 +1,11 @@
 import * as vscode from "vscode";
-import { IWebMsg, IChatMessage, IExtCommonMsg } from "./data";
+import {
+  IWebMsg,
+  IChatMessage,
+  IExtCommonMsg,
+  IWebChatMsg,
+  IWebChangeCurPromptMsg,
+} from "./data";
 import { gptManager } from "./GPTManager";
 import path = require("path");
 import fs = require("fs");
@@ -7,22 +13,18 @@ import { DEFAULT_PROMPTS } from "./prompts";
 
 function getWebViewContent(
   context: vscode.ExtensionContext,
-  templatePath: string
+  templatePath: string,
+  webView: vscode.Webview
 ) {
   const resourcePath = path.join(context.extensionPath, templatePath);
   const dirPath = path.dirname(resourcePath);
   let html = fs.readFileSync(resourcePath, "utf-8");
-  // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
+
   html = html.replace(
     /(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g,
     (m, $1, $2) => {
-      return (
-        $1 +
-        vscode.Uri.file(path.resolve(dirPath, $2))
-          .with({ scheme: "vscode-resource" })
-          .toString() +
-        '"'
-      );
+      const newPath = vscode.Uri.file(path.resolve(dirPath, $2));
+      return $1 + webView.asWebviewUri(newPath) + '"';
     }
   );
   return html;
@@ -83,34 +85,45 @@ class UIManager {
     } as IExtCommonMsg);
   };
 
+  private handleChatMessage = (message: IWebChatMsg) => {
+    const { data } = message;
+    this._messages = [
+      { role: "user", content: data.content, status: "success" },
+      { role: "assistant", content: "", status: "pending" },
+    ];
+    this.sendMsgToWebView();
+    gptManager
+      .getSingleCompletion(this._curPrompt, data.content)
+      .then((res) => {
+        this._messages[1].content = res;
+        this._messages[1].status = "success";
+        this.sendMsgToWebView();
+      })
+      .catch((err: Error) => {
+        this._messages[1].content = err.message;
+        this._messages[1].status = "error";
+        this.sendMsgToWebView();
+      });
+  };
+
+  private handleChangeCurPrompt = (message: IWebChangeCurPromptMsg) => {
+    const { data } = message;
+    this._curPromptKey = data.key;
+    this.sendMsgToWebView();
+    vscode.workspace
+      .getConfiguration("fe-dev-tools")
+      .update("curPromptKey", this._curPromptKey);
+  };
+
   onDidReceiveWebviewMessage = (message: IWebMsg) => {
-    const { command, data } = message;
+    console.log("message====>", message);
+    const { command } = message;
     switch (command) {
       case "chat":
-        this._messages = [
-          { role: "user", content: data.content, status: "success" },
-          { role: "assistant", content: "", status: "pending" },
-        ];
-        this.sendMsgToWebView();
-        gptManager
-          .getSingleCompletion(this._curPrompt, data.content)
-          .then((res) => {
-            this._messages[1].content = res;
-            this._messages[1].status = "success";
-            this.sendMsgToWebView();
-          })
-          .catch((err: Error) => {
-            this._messages[1].content = err.message;
-            this._messages[1].status = "error";
-            this.sendMsgToWebView();
-          });
+        this.handleChatMessage(message as IWebChatMsg);
         break;
       case "change-cur-prompt":
-        this._curPromptKey = data.key;
-        this.sendMsgToWebView();
-        vscode.workspace
-          .getConfiguration("fe-dev-tools")
-          .update("curPromptKey", this._curPromptKey);
+        this.handleChangeCurPrompt(message as IWebChangeCurPromptMsg);
         break;
     }
   };
@@ -123,16 +136,18 @@ class UIManager {
       this._currentPanel.reveal(columnToShowIn);
     } else {
       this._currentPanel = vscode.window.createWebviewPanel(
-        "catCoding",
+        "fe-dev-tools",
         "开发小工具",
         vscode.ViewColumn.Beside,
         {
           enableScripts: true,
+          retainContextWhenHidden: true,
         }
       );
       this._currentPanel.webview.html = getWebViewContent(
         this._context,
-        "src/siderbar.html"
+        "source/siderbar.html",
+        this._currentPanel.webview
       );
       this._currentPanel.webview.onDidReceiveMessage(
         this.onDidReceiveWebviewMessage,
